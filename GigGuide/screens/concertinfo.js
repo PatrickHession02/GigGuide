be159fd2b3106a1a9e52ccc371f96c0d1be28278
PATRICK HESSION <G00394619@gmit.ie>
2024-04-08 06:37:48 +0100

    Refactor concertinfo.js to add image overlay and adjust image dimensions

--- GigGuide/screens/concertinfo.js
+++ GigGuide/screens/concertinfo.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Text, ScrollView, StyleSheet, Dimensions } from 'react-native';
+import { Text, ScrollView, StyleSheet, Dimensions, Image, View } from 'react-native';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
-import FullWidthImage from 'react-native-fullwidth-image';
 
 const Concertinfo = ({ route }) => {
  const { concert } = route.params;
  const insets = useSafeAreaInsets(); // Get the safe area insets
 
  const styles = StyleSheet.create({
-
     gradient: {
       position: 'absolute',
       left: 0,
@@ -19,7 +17,22 @@ const Concertinfo = ({ route }) => {
     },
     scrollView: {
       width: "100%" // Make the ScrollView take up the full width of the screen
-
+    },
+    image: {
+      width: '100%', // Ensure the image takes the full width
+      height: 400, // Set the desired height
+      resizeMode: 'cover', // Cover the area without stretching
+    },
+    overlay: {
+      position: 'absolute', // Position the text absolutely
+      bottom: 10, // Position it 10px from the bottom
+      left: 10, // Position it 10px from the left
+      color: 'white', // Make the text color white so it's visible on the image
+      fontSize: 24, // Increase the font size
+      fontWeight: 'bold', // Make the text bold
+      backgroundColor: 'rgba(0, 0, 0, 0.5)', // Add a semi-transparent black background to the text
+      padding: 10, // Add some padding so the background doesn't hug the text too tightly
+      borderRadius: 5, // Add some border radius to make the background rounded
     },
  });
 
@@ -30,13 +43,16 @@ const Concertinfo = ({ route }) => {
     <>
       <LinearGradient colors={['#fc4908', '#fc0366']} style={styles.gradient} />
       <ScrollView contentContainerStyle={styles.container} style={styles.scrollView}>
-        {highestQualityImage && (
-     <FullWidthImage
-     source={{ uri: highestQualityImage.url }}
-     resizeMode="cover"
-   />
-        )}
-        <Text> {concert.name}</Text>
+        <View>
+          {highestQualityImage && (
+            <Image
+              source={{ uri: highestQualityImage.url }}
+              style={styles.image}
+              resizeMode="cover"
+            />
+          )}
+          <Text style={styles.overlay}> {concert.name}</Text>
+        </View>
         <Text>Concert Info</Text>
       </ScrollView>
     </>
