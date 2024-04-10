ab9d8f10f83c4d623085d75278c8b68c1007d6e2
PATRICK HESSION <G00394619@gmit.ie>
2024-04-10 04:56:45 +0100

    worked on AI testing

--- SpotifyAuthPractice/index.js
+++ SpotifyAuthPractice/index.js
@@ -9,6 +9,9 @@ const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const sessionSecret = process.env.SESSION_KEY;
 const rateLimit = require('axios-rate-limit');
 const http = rateLimit(axios.create(), { maxRequests: 5, perMilliseconds: 1000 });
+const aiTest = require('./OpenAiTEST');
+
+app.use('/ai', aiTest.routes);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://</gigguide-b3d86>.firebaseio.com'
