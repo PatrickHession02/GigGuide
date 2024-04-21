7e54fbe0a44b949a56953b6a9231e4a9d5eea861
PATRICK HESSION <G00394619@gmit.ie>
2024-04-21 07:55:31 +0100

    rolled back

--- GigGuide/screens/AI.js
+++ GigGuide/screens/AI.js
@@ -7,131 +7,137 @@ import { LinearGradient } from 'expo-linear-gradient';
 const AI = () => {
   const [data, setData] = useState([]);
   const navigation = useNavigation();
-  const [concertsData, setConcertsData] = useState([]);
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const responseConcerts = await fetch('https://8cbc-79-140-211-73.ngrok-free.app/AI');
-        const dataConcerts = await responseConcerts.json();
-        console.log('Fetched data:', dataConcerts);
-        if (!dataConcerts) {
-          console.error('Fetched data is undefined');
-          return;
+    fetch('https://8cbc-79-140-211-73.ngrok-free.app/AI')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Fetched AI data:', data);
+        if (data) {
+          const groupedData = data.reduce((acc, concert) => {
+            const artistName = concert.name;
+            if (!acc[artistName]) {
+              acc[artistName] = [];
+            }
+            acc[artistName].push(concert);
+            return acc;
+          }, {});
+          setData(groupedData);
         }
-        const groupedData = dataConcerts.reduce((acc, concert) => {
-          console.log('Current AI concert:', concert);
-          const artistIndex = acc.findIndex(artist => artist.name === concert.name);
-          if (artistIndex !== -1) {
-            acc[artistIndex].concerts.push(concert);
-          } else {
-            acc.push({ name: concert.name, concerts: [concert] });
-          }
-          return acc;
-        }, []);
-        setData(groupedData);
-      } catch (error) {
-        console.error('Error fetching concerts:', error);
-      }
-    };
-    fetchData();
+      });
   }, []);
 
-  const handleConcertPress = (concert) => {
-    navigation.navigate('Concertinfo', { concert });
+  const dataArray = Object.keys(data).map(key => ({
+    name: key,
+    concerts: data[key],
+  }));
+
+  const renderItem = ({ item: artist }) => {
+    const concert = artist.concerts[0];
+    if (concert && concert.images && concert.images.length > 0) {
+      return (
+        <TouchableOpacity onPress={() => handleConcertPress(artist)}>
+          <View style={styles.concertContainer}>
+            <View style={styles.imageContainer}>
+              <Image style={styles.concertImage} source={{ uri: concert.images[0].url }} />
+              <Text style={styles.concertName}>{concert.name}</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+    return null;
   };
 
-  const renderItem = ({ item: concert }) => {
-    const firstConcert = concert.concerts[0];
-    return (
-      <TouchableOpacity onPress={() => handleConcertPress(concert)}>
-        <LinearGradient
-          colors={['#4c669f', '#3b5998', '#192f6a']}
-          style={styles.concertContainer}
-        >
-          <Image source={{ uri: firstConcert.image }} style={styles.concertImage} />
-          <Text style={styles.concertName}>{firstConcert.name}</Text>
-        </LinearGradient>
-      </TouchableOpacity>
-    );
+  const handleConcertPress = (artist) => {
+    navigation.navigate('Concertinfo', { concerts: artist.concerts });
   };
 
-  const styles = StyleSheet.create({
-    gradient: {
-      flex: 1,
-    },
-    searchBarContainer: {
-      backgroundColor: 'transparent',
-      borderBottomColor: 'transparent',
-      borderTopColor: 'transparent',
-      paddingHorizontal: 16,
-      marginBottom: 10,
-    },
-    searchBarInputContainer: {
-      backgroundColor: '#fff',
-    },
-    scrollViewContainer: {
-      alignItems: 'center',
-      paddingVertical: 16,
-    },
-    concertContainer: {
-      width: 400, 
-      height: 300,
-      padding: 10,
-      borderRadius: 20,
-      backgroundColor: '#fff',
-      marginBottom: 20,
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-      borderWidth: 2,
-      borderColor: '#f205e2',
-    },
-    concertName: {
-      position: 'absolute',
-      bottom: 10,
-      left: 10,
-      color: 'white',
-      fontSize: 16,
-      fontWeight: 'bold',
-      textShadowColor: 'rgba(0, 0, 0, 0.75)',
-      textShadowOffset: { width: 2, height: 1 },
-      textShadowRadius: 80,
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      padding: 10,
-      borderRadius: 5,
-    },
-    concertImage: {
-      width: '100%',
-      height: '100%',
-      resizeMode: 'cover',
-      borderRadius: 10,
-    },
-    greetingText: {
-      fontSize: 32,
-      fontWeight: 'bold',
-      color: '#fff',
-      alignSelf: 'flex-start',
-      marginLeft: 16,
-      marginTop: 1,
-      textShadowColor: '#000',
-      textShadowOffset: { width: -1, height: 1 },
-      textShadowRadius: 10,
-      marginBottom: 20, 
-    },
-  });
-
+  console.log('Concerts Data:', data);
   return (
-    <FlatList
-      data={data}
-      renderItem={renderItem}
-      keyExtractor={(item, index) => index.toString()}
-    />
+    <LinearGradient colors={['#fc4908', '#fc0366']} style={styles.gradient}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Text style={styles.greetingText}>AI recommendations for you...</Text>
+        <FlatList
+          contentContainerStyle={styles.scrollViewContainer}
+          data={dataArray}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
+
+// Styles go here...
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+  },
+  searchBarContainer: {
+    backgroundColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderTopColor: 'transparent',
+    paddingHorizontal: 16,
+    marginBottom: 10,
+  },
+  searchBarInputContainer: {
+    backgroundColor: '#fff',
+  },
+  scrollViewContainer: {
+    alignItems: 'center',
+    paddingVertical: 16,
+  },
+  concertContainer: {
+    width: 400, 
+    height: 300,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: '#fff',
+    marginBottom: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    borderWidth: 2,
+    borderColor: '#f205e2',
+  },
+  concertName: {
+    position: 'absolute',
+    bottom: 10,
+    left: 10,
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: 2, height: 1 },
+    textShadowRadius: 80,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 10,
+    borderRadius: 5,
+  },
+  concertImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    borderRadius: 10,
+  },
+  greetingText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#fff',
+    alignSelf: 'flex-start',
+    marginLeft: 16,
+    marginTop: 1,
+    textShadowColor: '#000',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+    marginBottom: 20, 
+  },
+});
 
 export default AI;
