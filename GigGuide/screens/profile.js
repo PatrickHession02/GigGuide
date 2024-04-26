b9271ee3c742d15497957b2056f93a306c5788af
PATRICK HESSION <G00394619@gmit.ie>
2024-04-26 23:45:27 +0100

    Refactor profile screen and add image selection functionality in profile.js

--- GigGuide/screens/profile.js
+++ GigGuide/screens/profile.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import * as ImagePicker from 'expo-image-picker';
 
 const ProfileScreen = () => {
+  const [profileImage, setProfileImage] = useState(null);
+
+  const selectImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Sorry, we need camera roll permissions to make this work!');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setProfileImage(result.uri);
+    }
+  };
+
   return (
     <LinearGradient colors={['#fc4908', '#fc0366']} style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
         <Text style={styles.text}>Profile</Text>
+        <TouchableOpacity onPress={selectImage}>
+          {profileImage ? (
+            <Image source={{ uri: profileImage }} style={styles.profileImage} />
+          ) : (
+            <Text style={styles.addImageText}>Add Image</Text>
+          )}
+        </TouchableOpacity>
       </SafeAreaView>
     </LinearGradient>
   );
@@ -18,16 +50,27 @@ const styles = StyleSheet.create({
   },
   safeArea: {
     flex: 1,
-    justifyContent: 'flex-start', 
+    justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    paddingLeft: 10, 
+    paddingLeft: 10,
   },
   text: {
     fontSize: 30,
     color: '#fff',
     paddingLeft: 30,
     paddingTop: 20,
-    fontWeight: 'bold', 
+    fontWeight: 'bold',
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginTop: 20,
+  },
+  addImageText: {
+    fontSize: 20,
+    color: '#fff',
+    marginTop: 20,
   },
 });
 
