70f2804a20d668d793247699a7526c5bb27bc7a0
PATRICK HESSION <G00394619@gmit.ie>
2024-04-26 23:33:44 +0100

    Refactor profile screen and update styles in profile.js

--- GigGuide/screens/profile.js
+++ GigGuide/screens/profile.js
@@ -1,156 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { ScrollView, Text, StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { SearchBar } from 'react-native-elements';
-import { FIREBASE_AUTH } from '../FirebaseConfig';
-class Profile extends React.Component {
-  state = {
-    search: '',
-    profilePicUrl: null,
-  };
 
-  componentDidMount() {
-    FIREBASE_AUTH.onAuthStateChanged((user) => {
-      console.log(user); // Add this line
-      if (user) {
-        this.setState({ profilePicUrl: user.photoURL });
-      }
-    });
-  }
-
-
-  updateSearch = (search) => {
-    this.setState({ search });
-  };
-
-  render() {
-    const { search, profilePicUrl } = this.state;
-
-    return (
-      <LinearGradient colors={['#8E00FD', '#FF000F']} style={styles.gradient}>
-        <ScrollView contentContainerStyle={styles.container}>
-          <SearchBar
-            placeholder="Type Here..."
-            onChangeText={this.updateSearch}
-            value={search}
-            containerStyle={styles.searchBarContainer}
-            inputContainerStyle={styles.searchBarInputContainer}
-          />
-          <Text style={styles.profileHeaderText}>Profile</Text>
-          <View style={styles.horizontalLine} />
-          <View style={styles.profileContainer}>
-            <View style={styles.profilePictureContainer}>
-              <Image
-                source={profilePicUrl ? { uri: profilePicUrl } : require('../assets/ProfilePicExample.png')}
-                style={styles.profilePicture}
-              />
-            </View>
-            <View style={styles.profileTextContainer}>
-              <Text style={styles.usernameText}>Username</Text>
-              <Text style={styles.friendsText}>Friends</Text>
-              <Text style={styles.followedText}>Artists Followed</Text>
-            </View>
-          </View>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>Concert Wishlist</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>Reviews</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.separatorLine} />
-        </ScrollView>
-      </LinearGradient>
-    );
-  }
-}
+const ProfileScreen = () => {
+  return (
+    <LinearGradient colors={['#fc4908', '#fc0366']} style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        <Text style={styles.text}>Profile Screen</Text>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
 
 const styles = StyleSheet.create({
-  gradient: {
-    flex: 1,
-  },
   container: {
-    padding: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  searchBarContainer: {
-    backgroundColor: 'transparent',
-    borderBottomColor: 'transparent',
-    borderTopColor: 'transparent',
-    paddingHorizontal: 0,
-    width: '100%',
-  },
-  searchBarInputContainer: {
-    backgroundColor: '#fff',
-  },
-  horizontalLine: {
-    borderBottomColor: '#fff',
-    borderBottomWidth: 1,
-    width: '100%',
-    marginVertical: 10,
-  },
-  profileContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  profilePictureContainer: {
-    marginRight: 125,
-    alignItems: 'center',
-  },
-  profileTextContainer: {
     flex: 1,
-    justifyContent: 'center',
   },
-  profilePicture: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
+  safeArea: {
+    flex: 1,
+    justifyContent: 'flex-start', // Align items to the start of the main axis
+    alignItems: 'flex-start', // Align items to the start of the cross axis
+    paddingLeft: 10, // Add some padding to the left
   },
-  usernameText: {
-    color: '#fff',
-    fontSize: 18,
-  },
-  friendsText: {
-    color: '#fff',
-    fontSize: 14,
-    marginTop: 5,
-  },
-  followedText: {
-    color: '#fff',
-    fontSize: 14,
-    marginTop: 5,
-  },
-  profileHeaderText: {
-    color: '#fff',
+  text: {
     fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginTop: 20,
-    width: '100%',
-    justifyContent: 'space-evenly',
-  },
-  button: {
-    backgroundColor: '#fff',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-  },
-  buttonText: {
-    color: '#8E00FD',
-    fontSize: 16,
-  },
-  separatorLine: {
-    borderBottomColor: '#fff',
-    borderBottomWidth: 1,
-    width: '100%',
-    marginVertical: 10,
+    color: '#fff',
   },
 });
 
-export default Profile;
+export default ProfileScreen;
