0775ab2bbb2573859fa45cecb5bcf03e62bc0c5a
PATRICK HESSION <G00394619@gmit.ie>
2024-04-27 21:53:05 +0100

    Update API endpoints in HomeScreen.js and concertinfo.js

--- GigGuide/screens/concertinfo.js
+++ GigGuide/screens/concertinfo.js
@@ -5,8 +5,9 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 const Concertinfo = ({ route }) => {
  const { concert } = route.params;
+
  const insets = useSafeAreaInsets(); // Get the safe area insets
-  console.log("routes",route.params);
+
     const styles = StyleSheet.create({
     gradient: {
       position: 'absolute',
@@ -16,7 +17,7 @@ const Concertinfo = ({ route }) => {
       height: '100%',
     },
     scrollView: {
-      width: "100%" // Make the ScrollView take up the full width of the screen
+      width: "100%" 
     },
     image: {
       width: '100%', // Ensure the image takes the full width
@@ -34,16 +35,23 @@ const Concertinfo = ({ route }) => {
       padding: 10, // Add some padding so the background doesn't hug the text too tightly
       borderRadius: 5, // Add some border radius to make the background rounded
     },
+    dateContainer: {
+      flexDirection: 'row', // Add this to make the dates display side by side
+      flexWrap: 'wrap', // Add this to wrap the dates to the next line if they overflow
+    },
+    dateText: {
+      marginRight: 10, // Add some margin to the right of each date
+    },
  });
 
  const allImages = concert && Array.isArray(concert.concerts) ? concert.concerts.flatMap(concert => concert.images) : [];
- // Filter out fallback images
+
  const nonFallbackImages = allImages.filter(image => !image.fallback);
- // Sort in descending order of quality
+
  nonFallbackImages.sort((a, b) => b.quality - a.quality);
- // Get the highest quality image
+
  const highestQualityImage = nonFallbackImages[0];
- console.log(concert);
+ 
  return (
     <>
       <LinearGradient colors={['#fc4908', '#fc0366']} style={styles.gradient} />
@@ -58,8 +66,17 @@ const Concertinfo = ({ route }) => {
           )}
           <Text style={styles.overlay}> {concert.name}</Text>
         </View>
-        <Text>Concert Info</Text>
-        <Text>{concert.date ? new Date(concert.date).toLocaleDateString() : 'Date not available'}</Text>
+        <View style={styles.dateContainer}>
+          {concert.concerts.map((concertItem, index) => {
+            return (
+              <View key={index}>
+                <Text style={styles.dateText}>
+                  {concertItem.date ? new Date(concertItem.date).toLocaleDateString() : 'Date not available'}
+                </Text>
+              </View>
+            );
+          })}
+        </View>
       </ScrollView>
     </>
  );
