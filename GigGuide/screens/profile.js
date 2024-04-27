80b35470d449cc33ea30b6f9f57974bc9e88b534
PATRICK HESSION <G00394619@gmit.ie>
2024-04-27 19:12:39 +0100

    Update Firebase configuration and add Firebase Storage functionality to profile screen in profile.js

--- GigGuide/screens/profile.js
+++ GigGuide/screens/profile.js
@@ -1,185 +1,87 @@
-import { useEffect, useState } from "react";
-import {
-  View,
-  TouchableOpacity,
-  Image,
-  FlatList,
-  Platform,
-} from "react-native";
-import { Uploading } from "../components/Uploading";
-import { Ionicons } from "@expo/vector-icons";
-import * as ImagePicker from "expo-image-picker";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
-import { db, storage } from "../FirebaseConfig";
-import { Video } from "expo-av";
-import { UploadingAndroid } from "../components/UploadingAndroid";
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, StyleSheet, Image, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Modal, Button } from 'react-native';
+const ProfileScreen = () => {
+  const [profileImage, setProfileImage] = useState(null);
 
-export default function Home() {
-  const [image, setImage] = useState("");
-  const [video, setVideo] = useState("");
-  const [progress, setProgress] = useState(0);
-  const [files, setFiles] = useState([]);
-  useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, "files"), (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === "added") {
-          console.log("New file", change.doc.data());
-          setFiles((prevFiles) => [...prevFiles, change.doc.data()]);
-        }
-      });
-    });
-    return () => unsubscribe();
-  }, []);
-
-  async function pickImage() {
-    let result = await ImagePicker.launchImageLibraryAsync({
+  const selectImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Sorry, we need camera roll permissions to make this work!');
+      return;
+    }
+  
+    const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [3, 4],
+      aspect: [1, 1],
       quality: 1,
     });
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-      // upload the image
-      await uploadImage(result.assets[0].uri, "image");
+  
+    if (!result.cancelled) {
+      setProfileImage(result.assets[0].uri);
     }
-  }
-
-  async function pickVideo() {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
-      allowsEditing: true,
-      aspect: [3, 4],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-      await uploadImage(result.assets[0].uri, "video");
-    }
-  }
-
-  async function uploadImage(uri, fileType) {
-    const response = await fetch(uri);
-    const blob = await response.blob();
-
-    const storageRef = ref(storage, "Stuff/" + new Date().getTime());
-    const uploadTask = uploadBytesResumable(storageRef, blob);
-
-    // listen for events
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        setProgress(progress.toFixed());
-      },
-      (error) => {
-        // handle error
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-          console.log("File available at", downloadURL);
-          // save record
-          await saveRecord(fileType, downloadURL, new Date().toISOString());
-          setImage("");
-          setVideo("");
-        });
-      }
-    );
-  }
-
-  async function saveRecord(fileType, url, createdAt) {
-    try {
-      const docRef = await addDoc(collection(db, "files"), {
-        fileType,
-        url,
-        createdAt,
-      });
-      console.log("document saved correctly", docRef.id);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
+  };
   return (
-    <View style={{ flex: 1 }}>
-      <FlatList
-        data={files}
-        keyExtractor={(item) => item.url}
-        renderItem={({ item }) => {
-          if (item.fileType === "image") {
-            return (
-              <Image
-                source={{ uri: item.url }}
-                style={{ width: "34%", height: 100 }}
-              />
-            );
-          } else {
-            return (
-              <Video
-                source={{
-                  uri: item.url,
-                }}
-                // videoStyle={{ borderWidth: 1, borderColor: "red" }}
-                rate={1.0}
-                volume={1.0}
-                isMuted={false}
-                resizeMode="cover"
-                shouldPlay
-                // isLooping
-                style={{ width: "34%", height: 100 }}
-                useNativeControls
-              />
-            );
-          }
-        }}
-        numColumns={3}
-        contentContainerStyle={{ gap: 2 }}
-        columnWrapperStyle={{ gap: 2 }}
-      />
-      {image &&
-        (Platform.OS === "ios" ? (
-          <Uploading image={image} video={video} progress={progress} />
-        ) : (
-          // Some features of blur are not available on Android
-          <UploadingAndroid image={image} video={video} progress={progress} />
-        ))}
-      <TouchableOpacity
-        onPress={pickImage}
-        style={{
-          position: "absolute",
-          bottom: 90,
-          right: 30,
-          width: 44,
-          height: 44,
-          backgroundColor: "black",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: 25,
-        }}
-      >
-        <Ionicons name="image" size={24} color="white" />
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={pickVideo}
-        style={{
-          position: "absolute",
-          bottom: 150,
-          right: 30,
-          width: 44,
-          height: 44,
-          backgroundColor: "black",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: 25,
-        }}
-      >
-        <Ionicons name="videocam" size={24} color="white" />
-      </TouchableOpacity>
-    </View>
+    <LinearGradient colors={['#fc4908', '#fc0366']} style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        <Text style={styles.text}>Profile</Text>
+        <View style={styles.imageView}>
+          <TouchableOpacity style={styles.imageButton} onPress={selectImage}>
+            {profileImage ? (
+              <Image source={{ uri: profileImage }} style={styles.profileImage} />
+            ) : (
+              <Text style={styles.addImageText}>Add Image</Text>
+            )}
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+    alignItems: 'center', // Center items along the cross axis
+    paddingLeft: 10,
+  },
+  text: {
+    fontSize: 30,
+    color: '#fff',
+    paddingLeft: 30,
+    paddingTop: 20,
+    fontWeight: 'bold',
+  },
+  imageView: {
+    flex: 1,
+    justifyContent: 'center', // Center items along the main axis
+  },
+  imageButton: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    backgroundColor: '#fff2',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  addImageText: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center', // Center the text
+  },
+});
+
+export default ProfileScreen;
