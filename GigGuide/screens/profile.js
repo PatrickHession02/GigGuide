014ee14d59ebefe0493236190a080f2341f1758e
PATRICK HESSION <G00394619@gmit.ie>
2024-04-27 17:15:41 +0100

    Add Firebase Storage functionality to profile screen in profile.js

--- GigGuide/screens/profile.js
+++ GigGuide/screens/profile.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import { SafeAreaView, Text, StyleSheet, Image, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Modal, Button } from 'react-native';
+import { FIREBASE_AUTH, FIREBASE_STORAGE } from '../FirebaseConfig'; 
+
 const ProfileScreen = () => {
   const [profileImage, setProfileImage] = useState(null);
 
@@ -22,8 +24,41 @@ const ProfileScreen = () => {
   
     if (!result.canceled) {
       setProfileImage(result.assets[0].uri);
+      const userUID = FIREBASE_AUTH.currentUser.uid;
+      const storageRef = FIREBASE_STORAGE.ref(`profilePictures/${userUID}.jpg`);
+      fetch(result.assets[0].uri)
+      .then(response => response.blob())
+      .then(blob => {
+        const task = storageRef.put(blob);
+        task.on('state_changed',
+          (snapshot) => {
+            // Progress (if needed)
+          },
+          (error) => {
+            console.error('Error uploading image:', error);
+          },
+          async () => {
+            try {
+              const url = await storageRef.getDownloadURL();
+              console.log('Image URL:', url);
+
+              // Update the user's profile with the image URL
+              const user = FIREBASE_AUTH.currentUser;
+              await user.updateProfile({
+                photoURL: url,
+              });
+
+              console.log('User profile updated successfully:', user);
+              setProfileImage(url); // Set the state with the new URL
+            } catch (error) {
+              console.error('Error updating user profile:', error);
+            }
+          }
+        );
+      });
     }
   };
+  
   return (
     <LinearGradient colors={['#fc4908', '#fc0366']} style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
