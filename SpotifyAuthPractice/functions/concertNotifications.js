ba35d79d0c02605311c5c25feef20db736685557
PATRICK HESSION <G00394619@gmit.ie>
2024-04-30 01:22:24 +0100

    Added ASCII header

--- SpotifyAuthPractice/functions/concertNotifications.js
+++ SpotifyAuthPractice/functions/concertNotifications.js
@@ -9,10 +9,9 @@ exports.sendNotificationOnNewConcert = functions.firestore
     const before = change.before.data();
     const after = change.after.data();
 
-    console.log("Before data:", before); // Log the before data
-    console.log("After data:", after); // Log the after data
+    console.log("Before data:", before); // Logging the before data
+    console.log("After data:", after); // Logging the after data
 
-    // Check if before and after data exist and concerts is an array
     if (
       before &&
       after &&
@@ -20,26 +19,21 @@ exports.sendNotificationOnNewConcert = functions.firestore
       Array.isArray(after.concerts) &&
       before.concerts.length < after.concerts.length
     ) {
-      // ...
-
-      // Send a POST request to your Express server with the new concert data
+      
       axios.post("https://adab-79-140-211-73.ngrok-free.app/new-concert", {
         concert: after.concerts[after.concerts.length - 1],
       });
 
-      console.log("Message:", message); // Log the message
-
+      console.log("Message:", message); 
       const chunks = expo.chunkPushNotifications([message]);
-      console.log("Chunks:", chunks); // Log the chunks
+      console.log("Chunks:", chunks); 
 
       const sendPromises = chunks.map((chunk) =>
         expo.sendPushNotificationsAsync(chunk),
       );
-      console.log("Sending notifications"); // Log before sending notifications
-
+      console.log("Sending notifications"); 
       await Promise.all(sendPromises);
 
-      // Log a message when a new concert is added and a notification is sent
       console.log(
         `New concert ADDED FROM TEST: ${after.concerts[after.concerts.length - 1]}. Notification sent.`,
       );
